6eb753abf537d6c921b254be70a164816123b896
jamil noyda <jamilnoyda@gmail.com>
2022-05-12 06:10:43 +0000

    wip

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,44 @@
 <template>
-  <div >
-newEntries: {{ newEntries }}
-<table>
-    <tr v-for="(fund, index) in defaultFunds">
-      <td>{{ fund.name }}</td>
-      <td>
-       <input    v-model="newEntries[index].id"
-                 name="entryFund"
-                 :value="fund.id"
-                
-                 type="text"  />
-      </td>
-      <td>
-       <input  v-model="newEntries[index].amount"
-                 name="entryFund" 
-                 type="text"  />
-      </td>
-    </tr>
-</table>
-
-</div>
-
+  <div>
+    newEntries: {{ newEntries }}
+    <table>
+      <tr v-for="(fund, index) in defaultFunds" :key="index">
+      
+        <td>{{ fund.name }}</td>
+        <td>
+          <input v-model="newEntries[index].id" name="entryFund" type="text" />
+        </td>
+        <td>
+          <input
+            v-model="newEntries[index].amount"
+            name="entryFund"
+            type="text"
+          />
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script setup>
- export default {
+export default {
   data() {
-  
-  defaultFunds: [
+    return {
+      newEntries: [{}, {}],
+
+    defaultFunds: [
       {
         id: 100,
-        name: 'fund 0'
+        name: "fund 0",
       },
       {
         id: 101,
-        name: 'fund 1'
-      }
-  ],
-  newEntries: [{}, {}]
-},
-  methods: {
-  }
-   }
-  
-  
+        name: "fund 1",
+      },
+    ]
+    }
+    
+  },
+  methods: {},
+};
 </script>
